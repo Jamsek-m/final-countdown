12c32c760ba062c0a68b1e6da7684ef55ba98d73
Miha Jamsek <miha_jamsek@windowslive.com>
2017-04-18 20:35:09 +0200

    timer2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -45,19 +45,28 @@ namespace FinalCountdown {
 				vhod_sekunda.Text = input.Seconds.ToString();
 			}
 
+			timer = new DispatcherTimer();
+			timer.Tick += Timer_tick;
+			timer.Interval = new TimeSpan(0, 0, 1);
+
 		}
 
 		//se izvede vsako sekundo
 		private void Timer_tick(object sender, EventArgs e) {
+			this.time = this.time.Subtract(new TimeSpan(0, 0, 1));
+			vhod_ura.Text = this.time.Hours.ToString();
+			vhod_minuta.Text = this.time.Minutes.ToString();
+			vhod_sekunda.Text = this.time.Seconds.ToString();
 			if (this.time == new TimeSpan(0, 0, 0)) {
-				MessageBox.Show("Timeout over!", "End!");
-				timer.Stop();
-			} else {
-				this.time = this.time.Subtract(new TimeSpan(0, 0, 1));
-				vhod_ura.Text = this.time.Hours.ToString();
-				vhod_minuta.Text = this.time.Minutes.ToString();
-				vhod_sekunda.Text = this.time.Seconds.ToString();
+				konecCounta();
 			}
+		}
+
+		private void konecCounta() {
+			timer.Stop();
+			System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Windows\Media\Windows Proximity Notification.wav");
+			player.Play();
+			MessageBox.Show("Timeout over!", "End!");
 		}
 
 		private void gumb_calc_Click(object sender, RoutedEventArgs e) {
@@ -76,6 +85,7 @@ namespace FinalCountdown {
 			int m = Int32.Parse(vhod_minuta.Text);
 			int s = Int32.Parse(vhod_sekunda.Text);
 			this.time = new TimeSpan(H, m, s);
+
 
 			timer.Start();
 		}
